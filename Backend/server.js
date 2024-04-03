e610f131898128784b90143721966d59e307afe6
Maryna Melnyk <melnikm535@gmail.com>
2024-04-03 09:58:28 +0300

    2 commit

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const mysql = require('mysql')
+const mysql = require('mysql2')
 const cors = require('cors')
 
 const app = express()
@@ -8,16 +8,17 @@ app.use(cors())
 const db = mysql.createConnection({
     host: "localhost",
     user: 'root',
-    password: '',
+    password: 'melnykbdSQL2024',
     database: 'supermarket'
-})
+});
+
 
 app.get('/', (re, res)=>{
     return res.json("From Backend Side");
 })
 
 app.get('/users', (req, res)=>{
-    const sql = "SELECT * FROM employee";
+    const sql = "SELECT id_employee, empl_surname, empl_name FROM employee";
     db.query(sql, (err, data)=>{
         if(err) return res.json(err);
         return res.json(data);
